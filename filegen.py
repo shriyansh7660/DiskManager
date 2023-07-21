50b1ac62d55efc6f67b88d2e033f2a830fcc9ab9
ShreyanshNarayan <sriansh.ara@gmail.com>
2023-07-21 20:36:38 +0530

    adding python file to generate random sizes files for testing

--- filegen.py
+++ filegen.py
@@ -9,7 +9,7 @@ def generate_text_file(file_name, file_size):
 
 # Create 5 text files with random sizes between 1 MB and 10 MB
 for i in range(1, 4):
-    file_name = f'test_file.txt'
+    file_name = f'test_file_{i}.txt'
     file_size = random.randint(1, 10) * 1024 * 1024  # 1 MB to 10 MB
     generate_text_file(file_name, file_size)
     print(f'Created {file_name} with size {file_size} bytes.')
